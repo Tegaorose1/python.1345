33b393f9e92f549297a7920931d9e84945604822
Tegaorose1 <95704927+Tegaorose1@users.noreply.github.com>
2021-12-07 17:29:23 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,29 @@
+print('hello world')
+WHOLENUMBER = 5
+DECIMALNUMBER = 7.5
+COURSE = 'python'
+Y  = False
+
+print(WHOLENUMBER)
+print(DECIMALNUMBER)
+print(COURSE)
+print(Y)
+
+WORD = 'we\'re brothers from the other side of the town'
+print(WORD)
+POEM = 'he was fire,\nyet he didnt burn me,\ninstead he lightened me,\nwild fire still'
+print(POEM)
+print('\n')
+POEM2 = '''he was fire,
+yet he didnt bburn me,
+instead he lightened me,
+wild fire still
+'''
+print(POEM2)
+print('\n')
+print('lagos' + ' ' + 'state')#joining of string and string
+COURSE = 'python'
+print('Hey', COURSE, 'is fun')#joining of variable and string or variable and variable
+SURNAME = 'orose'
+FIRSTNAME = 'tega'
+print(SURNAME, FIRSTNAME)
---
+++ idle1 for.py
@@ -0,0 +1,175 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+Traceback (most recent call last):
+  File "C:/Users/USER/Documents/day1.py", line 12, in <module>
+    print (word = 'we\'re brothers from the other side of the town')
+TypeError: 'word' is an invalid keyword argument for print()
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didn,t burn me,
+instead he lightened me,
+wild fire still
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ===================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didnt burn me,
+instead he lightened me,
+wild fire still
+>>> 
+====================================== RESTART: C:/Users/USER/Documents/day1.py ======================================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didnt burn me,
+instead he lightened me,
+wild fire still
+he was fire,
+yet he didnt bburn me,
+instead he lightened me,
+wild fire still
+
+>>> 
+====================================== RESTART: C:/Users/USER/Documents/day1.py ======================================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didnt burn me,
+instead he lightened me,
+wild fire still
+
+
+he was fire,
+yet he didnt bburn me,
+instead he lightened me,
+wild fire still
+
+>>> 
+====================================== RESTART: C:/Users/USER/Documents/day1.py ======================================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didnt burn me,
+instead he lightened me,
+wild fire still
+
+
+he was fire,
+yet he didnt bburn me,
+instead he lightened me,
+wild fire still
+
+
+
+lagos state
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ==================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didnt burn me,
+instead he lightened me,
+wild fire still
+
+
+he was fire,
+yet he didnt bburn me,
+instead he lightened me,
+wild fire still
+
+
+
+lagos state
+Hey python is fun
+>>> 
+=================== RESTART: C:/Users/USER/Documents/day1.py ==================
+hello world
+5
+7.5
+python
+False
+we're brothers from the other side of the town
+he was fire,
+yet he didnt burn me,
+instead he lightened me,
+wild fire still
+
+
+he was fire,
+yet he didnt bburn me,
+instead he lightened me,
+wild fire still
+
+
+
+lagos state
+Hey python is fun
+orose tega
+>>> 
